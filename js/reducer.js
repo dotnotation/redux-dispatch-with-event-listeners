b7091dd02c45031706e8cdb6b244cdbae96f5f6f
dotnotation <dotinmotionproductions@gmail.com>
2022-02-17 13:42:41 -0500

    commit tree 393a6d6c20a0bcfb4e2e8c7149d14c06713b596b

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,31 @@
-// add code snippets from README
+let state
+
+function reducer(state = {count: 0}, action){
+    switch (action.type) {
+      case 'INCREASE_COUNT':
+        return {count: state.count + 1}
+      default:
+        return state;
+    }
+}
+// setting initial state as a default argument 
+
+function dispatch(action){
+    state = reducer(state, action);
+    render();
+}
+
+function render(){
+    let container = document.getElementById('container');
+    container.textContent = state.count;
+}
+
+dispatch({type: '@@INIT'})
+// returns the default state and renders it
+// if you remove this, your code won't break but the default state of 0 won't render on the page
+
+let button = document.getElementById('button');
+
+button.addEventListener('click', () => {
+  dispatch({type: 'INCREASE_COUNT'})
+})
